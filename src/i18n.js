c741a25c862f48790c1232f29db0f383080e0920
Andrey <andrejkapinskij@gmail.com>
2023-12-17 23:58:47 +0300

    i18nfix

--- src/i18n.js
+++ src/i18n.js
@@ -3,16 +3,29 @@ import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: 'ru',
-    debug: true,
-    detection: {
-        order: ['queryString', 'cookie'],
-        cache: ['cookie']
+import translationEN from "./locales/en/translation.json";
+import translationRU from "./locales/ru/translation.json";
+const resources = {
+    en: {
+        translation: translationEN,
     },
-    interpolation: {
-        escapeValue: false
-    }
-})
+    ru: {
+        translation: translationRU,
+    },
+};
+
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: "en",
+        debug: true,
+        lng: "ru",
+        resources,
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+    });
 
 export default i18n
